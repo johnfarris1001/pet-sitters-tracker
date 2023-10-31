1ef667320125dd5aee406edbf7efe5710a81a107
John <johnfarris217@gmail.com>
2023-10-30 19:49:19 -0500

    add sitter actions to controller

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -1,2 +1,23 @@
 class SittersController < ApplicationController
+    def index
+        sitters = Sitter.all
+        render json: sitters
+    end
+
+    def show
+        sitter = Sitter.find(params[:id])
+        render json: sitter
+    end
+
+    def create
+        sitter = Sitter.create(sitter_params)
+        render json: sitter, status: :created
+    end
+
+    private
+
+    def sitter_params
+        params.permit(:name, :has_home_with_yard, :phone_number, :address, :own_cats, :own_dogs)
+    end
+
 end
