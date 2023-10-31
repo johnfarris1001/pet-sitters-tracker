9ebfa7cf274c6687382473f3a38385079ee52775
John <johnfarris217@gmail.com>
2023-10-31 18:06:35 -0500

    add pet actions to controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,22 @@
 class PetsController < ApplicationController
+    def index
+        pets = Pet.all
+        render json: pets
+    end
+
+    def show
+        pet = Pet.find(params[:id])
+        render json: pet
+    end
+
+    def create
+        pet = Pet.create(pet_params)
+        render json: pet, status: :created
+    end
+
+    private
+
+    def pet_params
+        params.permit(:name, :species, :breed, :weight, :age, :notes)
+    end
 end
