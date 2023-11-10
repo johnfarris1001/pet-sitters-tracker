09923c3ad9b011071251df54f92c4374ff3aa5d4
John <johnfarris217@gmail.com>
2023-11-10 16:02:42 -0600

    add new pet form component

---
+++ client/src/components/NewPetForm.js
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+
+function NewPetForm({ addPet, showNewPetForm, setShowNewPetForm }) {
+    const [newPetInfo, setNewPetInfo] = useState({
+        name: "",
+        species: "",
+        breed: "",
+        weight: "",
+        age: 0,
+        notes: 0,
+    });
+
+    function handleNewPet(e) {
+        e.preventDefault();
+        fetch(`/pets`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPetInfo),
+        })
+            .then((r) => r.json())
+            .then((data) => addPet(data));
+        setShowNewPetForm(false);
+    }
+
+    const newPetDisplay = showNewPetForm
+        ? { width: "50%", margin: "auto", padding: "20px", border: "solid" }
+        : { display: "none" };
+
+    return (
+        <Form style={newPetDisplay} onSubmit={handleNewPet}>
+            <Form.Field>
+                <label>Name: </label>
+                <input
+                    value={newPetInfo.name}
+                    onChange={(e) =>
+                        setNewPetInfo({
+                            ...newPetInfo,
+                            name: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Species: </label>
+                <input
+                    value={newPetInfo.species}
+                    onChange={(e) =>
+                        setNewPetInfo({
+                            ...newPetInfo,
+                            species: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Breed: </label>
+                <input
+                    value={newPetInfo.breed}
+                    onChange={(e) =>
+                        setNewPetInfo({
+                            ...newPetInfo,
+                            breed: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Weight: </label>
+                <input
+                    value={newPetInfo.weight}
+                    onChange={(e) =>
+                        setNewPetInfo({
+                            ...newPetInfo,
+                            weight: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Age: </label>
+                <input
+                    value={newPetInfo.age}
+                    onChange={(e) =>
+                        setNewPetInfo({
+                            ...newPetInfo,
+                            age: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Notes: </label>
+                <input
+                    value={newPetInfo.notes}
+                    onChange={(e) =>
+                        setNewPetInfo({
+                            ...newPetInfo,
+                            notes: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Button>Submit</Button>
+        </Form>
+    );
+}
+
+export default NewPetForm;
--- client/src/components/NewSitterForm.js
+++ client/src/components/NewSitterForm.js
@@ -31,94 +31,92 @@ function NewSitterForm({ addSitter, showNewSitterForm, setShowNewSitterForm }) {
 
     return (
         <Form style={newSitterDisplay} onSubmit={handleNewSitter}>
-            <Form.Group>
-                <Form.Field>
-                    <label>Name: </label>
-                    <input
-                        value={newSitterInfo.name}
-                        onChange={(e) =>
-                            setNewSitterInfo({
-                                ...newSitterInfo,
-                                name: e.target.value,
-                            })
-                        }
-                    />
-                </Form.Field>
-                <Form.Group inline>
-                    <label>Has Yard At Home?</label>
-                    <Form.Radio
-                        label="Yes"
-                        checked={newSitterInfo.has_home_with_yard === true}
-                        onChange={(e) =>
-                            setNewSitterInfo({
-                                ...newSitterInfo,
-                                has_home_with_yard:
-                                    !newSitterInfo.has_home_with_yard,
-                            })
-                        }
-                    />
-                    <Form.Radio
-                        label="No"
-                        checked={newSitterInfo.has_home_with_yard === false}
-                        onChange={(e) =>
-                            setNewSitterInfo({
-                                ...newSitterInfo,
-                                has_home_with_yard:
-                                    !newSitterInfo.has_home_with_yard,
-                            })
-                        }
-                    />
-                </Form.Group>
-                <Form.Field>
-                    <label>Phone Number: </label>
-                    <input
-                        value={newSitterInfo.phone_number}
-                        onChange={(e) =>
-                            setNewSitterInfo({
-                                ...newSitterInfo,
-                                phone_number: e.target.value,
-                            })
-                        }
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <label>Address: </label>
-                    <input
-                        value={newSitterInfo.address}
-                        onChange={(e) =>
-                            setNewSitterInfo({
-                                ...newSitterInfo,
-                                address: e.target.value,
-                            })
-                        }
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <label>Number of Dogs: </label>
-                    <input
-                        value={newSitterInfo.own_dogs}
-                        onChange={(e) =>
-                            setNewSitterInfo({
-                                ...newSitterInfo,
-                                own_dogs: e.target.value,
-                            })
-                        }
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <label>Number of Cats: </label>
-                    <input
-                        value={newSitterInfo.own_cats}
-                        onChange={(e) =>
-                            setNewSitterInfo({
-                                ...newSitterInfo,
-                                own_cats: e.target.value,
-                            })
-                        }
-                    />
-                </Form.Field>
-                <Button>Submit</Button>
+            <Form.Field>
+                <label>Name: </label>
+                <input
+                    value={newSitterInfo.name}
+                    onChange={(e) =>
+                        setNewSitterInfo({
+                            ...newSitterInfo,
+                            name: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Group inline>
+                <label>Has Yard At Home?</label>
+                <Form.Radio
+                    label="Yes"
+                    checked={newSitterInfo.has_home_with_yard === true}
+                    onChange={(e) =>
+                        setNewSitterInfo({
+                            ...newSitterInfo,
+                            has_home_with_yard:
+                                !newSitterInfo.has_home_with_yard,
+                        })
+                    }
+                />
+                <Form.Radio
+                    label="No"
+                    checked={newSitterInfo.has_home_with_yard === false}
+                    onChange={(e) =>
+                        setNewSitterInfo({
+                            ...newSitterInfo,
+                            has_home_with_yard:
+                                !newSitterInfo.has_home_with_yard,
+                        })
+                    }
+                />
             </Form.Group>
+            <Form.Field>
+                <label>Phone Number: </label>
+                <input
+                    value={newSitterInfo.phone_number}
+                    onChange={(e) =>
+                        setNewSitterInfo({
+                            ...newSitterInfo,
+                            phone_number: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Address: </label>
+                <input
+                    value={newSitterInfo.address}
+                    onChange={(e) =>
+                        setNewSitterInfo({
+                            ...newSitterInfo,
+                            address: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Number of Dogs: </label>
+                <input
+                    value={newSitterInfo.own_dogs}
+                    onChange={(e) =>
+                        setNewSitterInfo({
+                            ...newSitterInfo,
+                            own_dogs: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Form.Field>
+                <label>Number of Cats: </label>
+                <input
+                    value={newSitterInfo.own_cats}
+                    onChange={(e) =>
+                        setNewSitterInfo({
+                            ...newSitterInfo,
+                            own_cats: e.target.value,
+                        })
+                    }
+                />
+            </Form.Field>
+            <Button>Submit</Button>
         </Form>
     );
 }
