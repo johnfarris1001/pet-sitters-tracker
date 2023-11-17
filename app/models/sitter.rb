6091e972f678abcf3c5614d4024ece39f64efe70
John <johnfarris217@gmail.com>
2023-11-16 18:44:30 -0600

    fix errors in validations

--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -1,7 +1,7 @@
 class Sitter < ApplicationRecord
     validates :name, presence: true
-    validates :phone_number, :presence: true
-    validates :address, :presences: true
+    validates :phone_number, presence: true
+    validates :address, presence: true
 
     has_many :appointments, dependent: :destroy
     has_many :pets, through: :appointments
